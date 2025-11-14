910b89c170efecedec05a5d41dd31c9fb5d622fb
neeby <artemboldirew2@gmail.com>
2025-11-14 23:48:38 +0300

    start commit

---
+++ src/main/java/ru/vsu/cs/boldyrev/meetme/MeetmeApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.boldyrev.meetme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/cs/boldyrev/meetme/MeetmeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.boldyrev.meetme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
